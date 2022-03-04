caae0c5d19a8581807a75a51b8d7934f58354d2b
Hafiz Raheel Siddique <siddiqueraheel216@gmail.com>
2022-03-04 12:44:15 +0500

    navbar done

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,9 +7,34 @@ const  Header=()=> {
   const toggleNavbar = () => setCollapsed(!collapsed);
   return (
     <div >
-    <Navbar className='headerportfolio'   light>
-      <NavbarBrand href="/" className="mr-auto">reactstrap</NavbarBrand>
-      <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+    <Navbar className='headerportfolio' light >
+      <NavbarBrand href="/" className="mr-auto"  style={{color:"rgb(176, 42, 253)"}}>
+            
+                  <ul className="mainlists">
+              
+                      <li className="imglogo">
+                      <img src="\209994540_177752471033893_7941265677326244466_n.jpg" width="250" height="140" alt="" />
+                      </li>
+                      <li className="listofheadernumber">+92 3122624846</li>
+                        
+                      <li className="listofheader">Home</li>
+                      <li className="listofheader">About Me</li>
+                      <li className="listofheader">Skills</li>
+                      <li className="listofheader">Work</li>
+                      <li className="listofheader">
+                          
+                    <button className='btn btn-outline-danger' style={{borderRadius:20}}>Contact Me</button>    
+                          
+                          </li>
+                   
+                  </ul>
+             
+                   
+                    
+           
+           
+      </NavbarBrand>
+      {/* <NavbarToggler onClick={toggleNavbar} className="mr-2" />
       <Collapse isOpen={!collapsed} navbar>
         <Nav navbar>
           <NavItem>
@@ -19,7 +44,7 @@ const  Header=()=> {
             <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
           </NavItem>
         </Nav>
-      </Collapse>
+      </Collapse> */}
     </Navbar>
   </div>
   )
